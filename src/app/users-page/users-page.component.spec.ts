603b4e5373d5a2fb6b0d8c9b9dc2577c158317f6
Vivekan97 <vivekanskb@gmail.com>
2022-07-26 10:53:14 +0530

    added test cases.

--- src/app/users-page/users-page.component.spec.ts
+++ src/app/users-page/users-page.component.spec.ts
@@ -1,6 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UsersService } from '../_services/users-service.service';
 
 import { UsersPageComponent } from './users-page.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+// import {  HttpClientModule  } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatTableModule  } from '@angular/material/table';
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {MatSortModule} from '@angular/material/sort';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 describe('UsersPageComponent', () => {
   let component: UsersPageComponent;
@@ -8,7 +22,13 @@ describe('UsersPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersPageComponent ]
+      declarations: [ UsersPageComponent ],
+      imports: [  
+        BrowserModule, BrowserAnimationsModule, HttpClientModule,
+        HttpClientModule, FormsModule, MatTooltipModule, MatProgressBarModule,
+        MatTableModule, MatPaginatorModule, MatSortModule, MatCheckboxModule
+      ],
+      providers: [UsersService]
     })
     .compileComponents();
   });
@@ -22,4 +42,21 @@ describe('UsersPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  
+  it(`should render 'ADMIN UI PAGE'`,() =>{
+    const fixture = TestBed.createComponent(UsersPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled.querySelector("h2")?.textContent);
+    expect(compiled.querySelector("h2")?.textContent).toContain("ADMIN UI PAGE");
+  })
+
+  it(`should create 'users-table'`,() =>{
+    const fixture = TestBed.createComponent(UsersPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    console.log(compiled.);
+    // expect(compiled.querySelector("h2")?.textContent).toContain("ADMIN UI PAGE");
+  })
 });
